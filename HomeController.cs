2ae66aa7571aacceb946c1cae3a0c93f74c766c6
nikoz95 <nikoloz.malidze@gmail.com>
2019-01-11 19:12:41 +0400

    meore

--- HomeController.cs
+++ HomeController.cs
@@ -21,11 +21,11 @@ namespace TextComparator.Controllers
         {
             ////////////////////////////////////////////////////////ORIGINAL
 
-            //List<TextRow> _items = new List<TextRow>();
-            //List<TextWord> _itemsWord = new List<TextWord>();
+            List<TextRow> _items = new List<TextRow>();
+            List<TextWord> _itemsWord = new List<TextWord>();
 
-            //List<TextRow> _itemsCH = new List<TextRow>();
-            //List<TextWord> _itemsWordCH = new List<TextWord>();
+            List<TextRow> _itemsCH = new List<TextRow>();
+            List<TextWord> _itemsWordCH = new List<TextWord>();
 
 
             string[] stringSeparators = new string[] { "\r\n" ," "};
@@ -33,104 +33,130 @@ namespace TextComparator.Controllers
             string[] textsOneRow = texts.stringOne.Split(new string[] { stringSeparators[0] }, StringSplitOptions.None);
             string[] textsTwoRow = texts.stringTwo.Split(new string[] { stringSeparators[0] }, StringSplitOptions.None);
 
-            string[,] text1 = new string[,]
-            {
-                {"რომელმან შექმნა სამყარო ძალითა მით ძლიერითა," },
-                {"ზეგარდმოთ არსნი სულითა ყვნა ზეცით მონაბერითა," },
-                {"ჩვენ, კაცთა, მოგვცა ქვეყანა, გვაქვს უთვალავი ფერითა," },
-                {"მისგან არს ყოვლი ხელმწიფე სახითა მის მიერითა" }
+            //string[,] text1 = new string[,]
+            //{
+            //    {"რომელმან შექმნა სამყარო ძალითა მით ძლიერითა," },
+            //    {"ზეგარდმოთ არსნი სულითა ყვნა ზეცით მონაბერითა," },
+            //    {"ჩვენ, კაცთა, მოგვცა ქვეყანა, გვაქვს უთვალავი ფერითა," },
+            //    {"მისგან არს ყოვლი ხელმწიფე სახითა მის მიერითა" }
 
-            };
+            //};
 
-            string[,,] text1Split = new string[,,]
-            {
-                {{"რომელმან" }, {"შექმნა" },{"სამყარო" },{"მით" },{"ძლიერითა" },{"მით" },{"ჩამატებული" }},
-                {{"ზეგარდმოთ" }, {"არსნი" },{"სულითა" },{"ყვნა" },{"ზეცით" },{"მონაბერითა" },{"ჩამატებული" }},
-                {{"ჩვენ" },{"კაცთა" },{"მოგვცა" },{"ქვეყანა" },{"გვაქვს" },{"უთვალავი" },{"ფერითა"}},
-                {{"მისგან" }, {"არს" },{"ყოვლი" },{"ხელმწიფე" },{"სახითა" },{"მის" },{"მიერითა" }}
+            //string[,,] text1Split = new string[,,]
+            //{
+            //    {{"რომელმან" }, {"შექმნა" },{"სამყარო" },{"მით" },{"ძლიერითა" },{"მით" },{"ჩამატებული" }},
+            //    {{"ზეგარდმოთ" }, {"არსნი" },{"სულითა" },{"ყვნა" },{"ზეცით" },{"მონაბერითა" },{"ჩამატებული" }},
+            //    {{"ჩვენ" },{"კაცთა" },{"მოგვცა" },{"ქვეყანა" },{"გვაქვს" },{"უთვალავი" },{"ფერითა"}},
+            //    {{"მისგან" }, {"არს" },{"ყოვლი" },{"ხელმწიფე" },{"სახითა" },{"მის" },{"მიერითა" }}
 
-            };
-
-
-
-            List<Result> _result = new List<Result>();
-
-            for (int i = 0; i < textsTwoRow.Length; i++)
-            {
-                string[] SplitRow = new string[] { };
-                string[] SplitRowCH = new string[] { };
-                //_items.Add(new TextRow { RowId = i, RowText = textsOneRow[i] });
-                if (textsOneRow[i].Count() < textsTwoRow[i].Count())
-                {
-                    
-                    SplitRowCH = textsTwoRow[i].Split(stringSeparators, StringSplitOptions.None);
-                    Array.Resize(ref textsOneRow, textsOneRow.Length + 1);
-                    textsOneRow[textsOneRow.Length - 1] = "new string new string new string new string new string new string new string";
-
-                }
-                else
-                {
-                    SplitRow = textsOneRow[i].Split(stringSeparators, StringSplitOptions.None);
-                    SplitRowCH = textsTwoRow[i].Split(stringSeparators, StringSplitOptions.None);
-
-                }
+            //};
+            //string[][] jaggedArray3 =
+            //{
+            //    new string[]{  "რომელმან შექმნა სამყარო ძალითა მით ძლიერითა,",  "რომელმან", "შექმნა", "სამყარო", "ძალითა", "მით", "ძლიერითა" },
+            //    new string[]{  "ზეგარდმოთ არსნი სულითა ყვნა ზეცით მონაბერითა," , "ზეგარდმოთ" , "არსნი" ,"სულითა" ,"ყვნა" ,"ზეცით" ,"მონაბერითა" },
+            //    new string[]{  "ჩვენ, კაცთა, მოგვცა ქვეყანა, გვაქვს უთვალავი ფერითა," ,  "ჩვენ" ,  "კაცთა" ,  "მოგვცა" , "ქვეყანა" ,  "გვაქვს" ,  "უთვალავი" ,  "ფერითა"  },
+            //    new string[]{  "მისგან არს ყოვლი ხელმწიფე სახითა მის მიერითა" , "მისგან" ,  "არს" ,  "ყოვლი" ,  "ხელმწიფე" ,  "სახითა" ,  "მის" ,  "მიერითა"  }
                 
-                try
-                {
-                    for (int j = 0; j < 50; j++)
-                    {
+            //};
+            //string[,] jaggedText = new string[,] { };
+
+            //for (int i = 0; i < textsOneRow.Length; i++)
+            //{
+            //    string[] SplitRowW = textsOneRow[i].Split(stringSeparators, StringSplitOptions.None);
+            //    for (int j = 0; j < SplitRowW.Length; j++)
+            //    {
+            //        string[,] jaggedText1 =   new string[,] {{ textsOneRow[i], SplitRowW[j] } };
+            //        jaggedText[i, 0] = textsOneRow[i];
+            //        jaggedText += jaggedText1;
+
+
+                    
+                    
+                    
+            //    }
+            //}
+
+
+            //List<Result> _result = new List<Result>();
+
+                //try
+                //{
+                        //for (int i = 0; i < 50; i++)
+                        //{
+                        //    string[] SplitRow = new string[] { };
+                        //    string[] SplitRowCH = new string[] { };
+                            //_items.Add(new TextRow { RowId = i, RowText = textsOneRow[i] });
+                            //if (textsOneRow[i].Count() < textsTwoRow[i].Count())
+                            //{
+                    
+                            //    SplitRowCH = textsTwoRow[i].Split(stringSeparators, StringSplitOptions.None);
+                            //    Array.Resize(ref textsOneRow, textsOneRow.Length + 1);
+                            //    textsOneRow[textsOneRow.Length - 1] = "new string new string new string new string new string new string new string";
+
+                            //}
+                            //else
+                            //{
+                                //SplitRow = textsOneRow[i].Split(stringSeparators, StringSplitOptions.None);
+                                //SplitRowCH = textsTwoRow[i].Split(stringSeparators, StringSplitOptions.None);
+
+                            //}
+                
+                                //for (int j = 0; j < 50; j++)
+                                //{
+
+                                    //if (SplitRow.Count() < SplitRowCH.Count())
+                                    //{
+                                    //    _result.Add(new Result
+                                    //    {
+                                    //        RowId = i,
+                                    //        RowTextOr = textsOneRow[i].Length > 0 ? textsOneRow[i] : "jj",
+                                    //        RowTextCh = textsTwoRow[i],
+                                    //        WordId = j,
+                                    //        WordOr = textsOneRow[i].Length > j ? SplitRow[j] : "<<WordOr",
+                                    //        WordCh = textsTwoRow[i].Length > j ? SplitRowCH[j] : "<<WordCh",
+                                    //        Status = "wrong",
+                                    //    });
+                                    //    k++;
+                                    //}
+                                    //else if (SplitRow.Count() > SplitRowCH.Count())
+                                    //{
+
+                                    //    _result.Add(new Result
+                                    //    {
+                                    //        RowId = i,
+                                    //        RowTextOr = textsOneRow[i].Length > 0 ? textsOneRow[i] : "jj",
+                                    //        RowTextCh = textsTwoRow[i],
+                                    //        WordId = j,
+                                    //        WordOr = textsOneRow[i].Length > j ? SplitRow[j] : "WordOr>>",
+                                    //        WordCh = textsTwoRow[i].Length > j ? SplitRowCH[j] : "WordCh>>",
+                                    //        Status = "wrong",
+                                    //    });
+
+
+                                    //}
+                                    //else {
+                       
+                                //_result.Add(new Result
+                                //    {
+                                //        RowId = i,
+                                //        RowTextOr = textsOneRow[i].Length > 0 ? textsOneRow[i] : "jj",
+                                //        RowTextCh = textsTwoRow[i],
+                                //        WordId = j,
+                                //        WordOr = textsOneRow[i].Length > j ? SplitRow[j] : "jj",
+                                //        WordCh = SplitRowCH[j],
+                                //        Status = SplitRowCH[j] == SplitRow[j] ? "correct" : "wrong",
+                                //    });
+
                         
-                        //if (SplitRow.Count() < SplitRowCH.Count())
-                        //{
-                        //    _result.Add(new Result
-                        //    {
-                        //        RowId = i,
-                        //        RowTextOr = textsOneRow[i].Length > 0 ? textsOneRow[i] : "jj",
-                        //        RowTextCh = textsTwoRow[i],
-                        //        WordId = j,
-                        //        WordOr = textsOneRow[i].Length > j ? SplitRow[j] : "<<WordOr",
-                        //        WordCh = textsTwoRow[i].Length > j ? SplitRowCH[j] : "<<WordCh",
-                        //        Status = "wrong",
-                        //    });
-                        //    k++;
+                                    //}
+                                //}
                         //}
-                        //else if (SplitRow.Count() > SplitRowCH.Count())
-                        //{
 
-                        //    _result.Add(new Result
-                        //    {
-                        //        RowId = i,
-                        //        RowTextOr = textsOneRow[i].Length > 0 ? textsOneRow[i] : "jj",
-                        //        RowTextCh = textsTwoRow[i],
-                        //        WordId = j,
-                        //        WordOr = textsOneRow[i].Length > j ? SplitRow[j] : "WordOr>>",
-                        //        WordCh = textsTwoRow[i].Length > j ? SplitRowCH[j] : "WordCh>>",
-                        //        Status = "wrong",
-                        //    });
-
-
-                        //}
-                        //else {
-
-                            _result.Add(new Result
-                            {
-                                RowId = i,
-                                RowTextOr = textsOneRow[i].Length > 0 ? textsOneRow[i] : "jj",
-                                RowTextCh = textsTwoRow[i],
-                                WordId = j,
-                                WordOr = textsOneRow[i].Length > j ? SplitRow[j] : "jj",
-                                WordCh = SplitRowCH[j],
-                                Status = SplitRowCH[j] == SplitRow[j] ? "correct" : "wrong",
-                            });
-                        //}
-                    }
-            }
-                    catch (System.IndexOutOfRangeException ex)
-                    {
-                        string input1 = "";
-                    }
-            }
-
+            //}
+            //catch (System.IndexOutOfRangeException ex)
+            //{
+            //    string input1 = "";
+            //}
             //gansazgvre udidesi
             //for (int i = 0; i < textsTwoRow.Length; i++)
             //{
@@ -143,11 +169,11 @@ namespace TextComparator.Controllers
             //}
             //for (int i = 0; i < _itemsCH.Count(); i++)
             //{
-                
+
             //    string[] split = _itemsCH[i].RowText.Split(stringSeparators, StringSplitOptions.None);
             //    for (int j = 0; j < split.Length; j++)
             //    {
-                      
+
             //        _result.Add(new Result
             //        {
             //            RowId = i,
@@ -173,8 +199,99 @@ namespace TextComparator.Controllers
             //                       Word = itemword.RowText
             //                      };
             ////////////////////////////////////////////////////////CHANGE
+            List<TextWord> _textOr = new List<TextWord>();
+            List<TextWord> _textCh = new List<TextWord>();
+            List<TextWord> mistake = new List<TextWord>();
+            List<TextWord> mistake1 = new List<TextWord>();
+            List<TextWord> group = new List<TextWord>();
+            List<List<TextWord>> diss = new List<List<TextWord>>();
+            //var settings = new List<string[]>();
+            List<List<String[]>> _SecLocations = new List<List<String[]>>{
+                
+                new List<String[]> { new string[]{"Item1", "Item2", "Item3", "Item4"}},
+                new List<String[]> {  new string[]{"Item1", "Item2", "Item3", "Item4"}},
+               
+            };
+            List<string> dis = new List<string>();
+
+            for (int i = 0; i < textsOneRow.Length; i++)
+            {
+                string[] split = textsOneRow[i].Split(stringSeparators, StringSplitOptions.None);
+                for (int j = 0; j <split.Length ; j++)
+                {
+                    _textOr.Add(new TextWord
+                    {
+                        RowIdFR = i,
+                        Text = textsOneRow[i],
+                        WordId = j,
+                        RowText = split[j]
+                    });
+                    //dis.Add(_textOr.ToString());
+                    diss.Add(_textOr);
+                }
+
+            }
+            for (int i = 0; i < textsTwoRow.Length; i++)
+            {
+                string[] split = textsTwoRow[i].Split(stringSeparators, StringSplitOptions.None);
+                for (int j = 0; j < split.Length; j++)
+                {
+                    _textCh.Add(new TextWord
+                    {
+                        RowIdFR = i,
+                        Text = textsTwoRow[i],
+                        WordId = j,
+                        RowText = split[j]
+                    });
+                }
+
+            }
+            var difList = _textOr.Where(a => !_textCh.Any(a1 => a1.RowText == a.RowText))
+            .Union(_textCh.Where(a => !_textOr.Any(a1 => a1.RowText == a.RowText)));
+            //var difLis1t = _textOr.Where(item => _textCh.Contains(item.RowText));
+           
+            for (int i = 0; i < _textOr.Count; i++)
+            {
+                if (!_textOr[i].RowText.Contains(_textCh[i].RowText))
+                {
+                    mistake.Add(_textCh[i]);
+                } 
+            }
+
+            //try
+            //{
+            //    for (int i = 0; i < _textCh.Count; i++)
+            //    {
+            //        //var groupedCustomerList = mistake.GroupBy(u => u.RowIdFR).Select(grp => grp.ToList()).ToList();
 
 
+
+            //        var l = textsOneRow[i].Count();
+            //        if (mistake[i].RowIdFR != _textOr[i+l].RowIdFR)
+            //        {
+
+            //            if (!_textOr[i].RowText.Contains(_textCh[i].RowText))
+            //            {
+            //                mistake1.Add(_textCh[i]);
+            //            }
+
+            //        }
+            //    }
+            //}
+            //catch (System.ArgumentOutOfRangeException ex)
+            //{
+            //    string input1 = "";
+            //}
+                    
+
+                        
+                    
+                
+               
+            
+        
+            Console.WriteLine(mistake);
+            Console.WriteLine(mistake1);
             ////////////////////////////////////////////////////////////////////////////////
             //var queryCH =
             //           from itemRow in _itemsCH
@@ -211,8 +328,8 @@ namespace TextComparator.Controllers
             // var list = query.Select().Concat(queryCH.Where(x => true));
 
 
-            
-            Console.WriteLine(_result);
+
+            // Console.WriteLine(_result);
 
 
 
